36b12f4a1c475a0c599174ec8aa2c03ea9b11e8e
Jacob Haip <haipjacob@gmail.com>
2018-05-05 22:17:54 -0400

    add projector calibration UI to camera program

--- programs/frame-to-dots.py
+++ programs/frame-to-dots.py
@@ -22,6 +22,11 @@ class ShowCapture(wx.Panel):
         self.bmp = wx.Bitmap.FromBuffer(width, height, frame)
         self.dots = []
 
+        self.projector_calibration_state = None
+        self.projector_calibration = [(50, 50), (CAM_WIDTH-50, 50),
+                                      (CAM_WIDTH-50, CAM_HEIGHT-50),
+                                      (50, CAM_HEIGHT-50)]
+
         self.M = RPCClient.RPCClient()
 
         self.timer = wx.Timer(self)
@@ -30,16 +35,46 @@ class ShowCapture(wx.Panel):
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         self.Bind(wx.EVT_TIMER, self.NextFrame)
 
+        self.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
+        self.Bind(wx.EVT_KEY_UP, self.OnKeyDown)
+        self.Bind(wx.EVT_CHAR, self.OnKeyDown)
+        self.Bind(wx.EVT_LEFT_UP, self.onClick)
+        self.SetFocus()
+
 
     def OnPaint(self, evt):
         dc = wx.BufferedPaintDC(self)
+        dc.SetBrush(wx.Brush())
+        font =  dc.GetFont()
+        font.SetWeight(wx.FONTWEIGHT_BOLD)
+        dc.SetFont(font)
+
         dc.DrawBitmap(self.bmp, 0, 0)
+
         for dot in self.dots:
             dc.SetBrush(wx.Brush(wx.Colour(dot["color"][0], dot["color"][1], dot["color"][2])))
             # dc.SetBrush(wx.Brush(wx.Colour(255, 0, 0)))
             dc.SetPen(wx.Pen(wx.Colour(255, 0, 0)))
             s = 3
             dc.DrawEllipse(int(dot["x"])-s, int(dot["y"])-s, s*2, s*2)
+
+        dc.SetBrush(wx.Brush(wx.Colour(0,255,255), style=wx.BRUSHSTYLE_TRANSPARENT))
+        dc.SetPen(wx.Pen(wx.Colour(0,0,255)))
+        dc.DrawPolygon(self.projector_calibration)
+
+        if self.projector_calibration_state is not None:
+            pt = self.projector_calibration[self.projector_calibration_state]
+            dc.SetBrush(wx.Brush(wx.Colour(0,255,255), style=wx.BRUSHSTYLE_TRANSPARENT))
+            dc.SetPen(wx.Pen(wx.Colour(255, 255, 255)))
+            s = 3
+            dc.DrawEllipse(int(pt[0])-s, int(pt[1])-s, s*2, s*2)
+            dc.SetPen(wx.Pen(wx.Colour(0, 0, 0)))
+            s = s + 1
+            dc.DrawEllipse(int(pt[0])-s, int(pt[1])-s, s*2, s*2)
+            dc.SetPen(wx.Pen(wx.Colour(255, 255, 255)))
+            s = s + 1
+            dc.DrawEllipse(int(pt[0])-s, int(pt[1])-s, s*2, s*2)
+            dc.DrawText("EDITING CORNER " + str(self.projector_calibration_state), CAM_WIDTH/2, CAM_HEIGHT/2)
 
     def NextFrame(self, event):
         ret, frame = self.capture.read()
@@ -83,6 +118,42 @@ class ShowCapture(wx.Panel):
             self.M.set_image(img_str)
 
             self.Refresh()
+
+    def moveCurrentCalibrationPointRel(self, dx, dy):
+        if self.projector_calibration_state is not None:
+            prev = self.projector_calibration[self.projector_calibration_state]
+            next = (prev[0] + dx, prev[1] + dy)
+            self.projector_calibration[self.projector_calibration_state] = next
+
+    def moveCurrentCalibrationPoint(self, pt):
+        if self.projector_calibration_state is not None:
+            self.projector_calibration[self.projector_calibration_state] = pt
+
+    def changeCurrentCalibrationPoint(self, key):
+        if key == '`':
+            self.projector_calibration_state = None
+        elif key in ['1', '2', '3', '4']:
+            self.projector_calibration_state = int(key)-1
+
+    def OnKeyDown(self, event=None):
+        keyCode = event.GetKeyCode()
+        if keyCode == wx.WXK_UP:
+            self.moveCurrentCalibrationPointRel(0, -1)
+        elif keyCode == wx.WXK_RIGHT:
+            self.moveCurrentCalibrationPointRel(1, 0)
+        elif keyCode == wx.WXK_DOWN:
+            self.moveCurrentCalibrationPointRel(0, 1)
+        elif keyCode == wx.WXK_LEFT:
+            self.moveCurrentCalibrationPointRel(-1, 0)
+        else:
+            unicodeKey = chr(event.GetUnicodeKey())
+            if unicodeKey in ['`', '1', '2', '3', '4']:
+                self.changeCurrentCalibrationPoint(unicodeKey)
+
+    def onClick(self, event=None):
+        if event:
+            pt = event.GetPosition()
+            self.moveCurrentCalibrationPoint(pt)
 
 
 capture = cv2.VideoCapture(0)
