2b005f8f2e90ff51a60ab3ef839c56e966ad1ea5
Jacob Haip <haipjacob@gmail.com>
2018-05-06 00:09:49 -0400

    use projector cal + cv perspectiveTransform so projection matches papers

--- programs/gui_projector.py
+++ programs/gui_projector.py
@@ -1,6 +1,8 @@
 import wx
+import cv2
 import RPCClient
 import logging
+import numpy as np
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,13 +17,16 @@ class Example(wx.Frame):
 
         self.Bind(wx.EVT_PAINT, self.OnPaint)
 
-        self.Centre()
+        # self.Centre()
         self.Show()
+        self.Maximize(True)
 
         self.i = 0
         self.bmp = None
         self.dots = []
         self.papers = []
+        self.projector_calibration = []
+        self.projection_matrix = None
 
         self.M = RPCClient.RPCClient()
 
@@ -44,17 +49,20 @@ class Example(wx.Frame):
         # dc.DrawText('Historical Prices', 90, 235)
 
         dc = wx.BufferedPaintDC(self)
+        dc.SetBackground(wx.Brush(wx.Colour(0,0,0)))
+        dc.Clear()
+        dc.SetTextForeground(wx.Colour(255,255,255))
         dc.SetBrush(wx.Brush())
         font =  dc.GetFont()
         font.SetWeight(wx.FONTWEIGHT_BOLD)
         dc.SetFont(font)
-        if self.bmp:
-            dc.DrawBitmap(self.bmp, 0, 0)
-        for dot in self.dots:
-            dc.SetBrush(wx.Brush(wx.Colour(dot["color"][0], dot["color"][1], dot["color"][2])))
-            dc.SetPen(wx.Pen(wx.Colour(255, 255, 0)))
-            s = 3
-            dc.DrawEllipse(int(dot["x"])-s, int(dot["y"])-s, s*2, s*2)
+        # if self.bmp:
+        #     dc.DrawBitmap(self.bmp, 0, 0)
+        # for dot in self.dots:
+        #     dc.SetBrush(wx.Brush(wx.Colour(dot["color"][0], dot["color"][1], dot["color"][2])))
+        #     dc.SetPen(wx.Pen(wx.Colour(255, 255, 0)))
+        #     s = 3
+        #     dc.DrawEllipse(int(dot["x"])-s, int(dot["y"])-s, s*2, s*2)
         if self.papers:
             for paper in self.papers:
                 dc.SetPen(wx.NullPen)
@@ -67,15 +75,22 @@ class Example(wx.Frame):
                             tri1.append(corner)
                         if corner["CornerId"] in [2,3,0]:
                             tri2.append(corner)
-                    dc.DrawPolygon(list(map(lambda c: (c["x"], c["y"]), tri1)))
-                    dc.DrawPolygon(list(map(lambda c: (c["x"], c["y"]), tri2)))
+                    dc.DrawPolygon(self.project(list(map(lambda c: (c["x"], c["y"]), tri1))))
+                    dc.DrawPolygon(self.project(list(map(lambda c: (c["x"], c["y"]), tri2))))
                 if len(paper["corners"]) == 3:
                     tri1 = paper["corners"]
-                    dc.DrawPolygon(list(map(lambda c: (c["x"], c["y"]), tri1)))
+                    pts = self.project(list(map(lambda c: [c["x"], c["y"]], tri1)))
+                    dc.DrawPolygon(pts)
                 for corner in paper["corners"]:
-                    dc.DrawText(paper["id"] + ": " + str(corner["CornerId"]), corner["x"], corner["y"])
+                    textPt = self.project([(corner["x"], corner["y"])])[0]
+                    dc.DrawText(paper["id"] + ": " + str(corner["CornerId"]), textPt[0], textPt[1])
 
-
+    def project(self, pts):
+        if self.projection_matrix is not None:
+            dst = cv2.perspectiveTransform(np.array([np.float32(pts)]), self.projection_matrix)
+            return list(map(lambda x: [int(x[0]), int(x[1])], dst[0]))
+        logging.error("MISSING PROJECTION MATRIX FOR PAPERS!")
+        return None
 
     def OnTimer(self, event):
         if event.GetId() == Example.ID_TIMER:
@@ -103,6 +118,25 @@ class Example(wx.Frame):
             self.M.when("global", "papers", receivePapers)
             if self.papers:
                 logging.info("got papers %s" % len(self.papers))
+
+            def receiveProjectorCalibration(cal):
+                self.projector_calibration = cal
+            self.M.when("global", "projector_calibration", receiveProjectorCalibration)
+            logging.info("got projector_calibration %s" % self.projector_calibration)
+
+            if self.projector_calibration and len(self.projector_calibration) is 4:
+                pts1 = np.float32(self.projector_calibration)
+                pts2 = np.float32([[0,0],[CAM_WIDTH,0],[CAM_WIDTH,CAM_HEIGHT],[0,CAM_HEIGHT]])
+                self.projection_matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+                logging.info("CONVERT PERSPECTIVE")
+                logging.info(self.projection_matrix)
+                logging.info(np.float32(self.projector_calibration))
+
+                dst = cv2.perspectiveTransform(np.array([np.float32(self.projector_calibration)]), self.projection_matrix)
+                logging.info("IDENTITY?")
+                logging.info(dst)
+                logging.info(list(map(lambda x: [int(x[0]), int(x[1])], dst[0])))
 
             self.bmp = wxImg.ConvertToBitmap()
 
