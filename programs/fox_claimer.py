3651da772f86e8afef6db2ae740bcf766cefd8bf
Jacob Haip <haipjacob@gmail.com>
2018-06-23 01:40:12 -0400

    update fox-conditional and claimer to work with pub/sub

--- programs/fox_claimer.py
+++ programs/fox_claimer.py
@@ -1,9 +1,15 @@
 import logging
 import sys
 import RPCClient
+import time
 
 logging.basicConfig(level=logging.INFO)
 M = RPCClient.RPCClient()
 
+time.sleep(1)
+# wait for a second for subscribers to be notified
+# of the socket subscription before sending data.
+# TODO: figure out how to make this not needed
+
 id = sys.argv[1]
-M.claim(id, "fox", "out")
+M.claim(str(id), "fox", "out")
--- programs/fox_conditional.py
+++ programs/fox_conditional.py
@@ -7,13 +7,23 @@ M = RPCClient.RPCClient()
 
 id = sys.argv[1]
 
+M.when_set_filter("CLAIM[1054/fox]")
+M.when_set_filter("DEATH[1054]")
 
-def when_fox(fox):
-    logging.info("Inside when fox")
-    if fox == "out":
-        M.wish("DRAW", id, "The fox is out!")
+while True:
+    string = M.when_recv()
+    event_type = string.split('[', 1)[0]  # CLAIM, DEATH, WISH
+    fox = None
+    if event_type == "DEATH":
+        fox = None
+    elif event_type == "CLAIM":
+        fox = string[len("CLAIM[1054/fox]"):]
+        logging.error(fox)
+    ill = M.new_illumination(id)
+    # "\"out\"" is used because it equals json.dumps("out")
+    # TODO: change RPCClient to not encode pure text as json
+    if fox == "\"out\"":
+        ill.text("The fox is out!", 40, 40)
     else:
-        M.clear_wishes({"source": id})
-
-
-M.when(1054, "fox", when_fox)
+        ill.text("Where is Mr. fox?", 10, 10)
+    M.wish("DRAW", id, ill.package())
